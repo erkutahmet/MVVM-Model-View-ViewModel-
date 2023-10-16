473eb66e484774a6e0024a0cbb1b37501fbed050
erkutahmet <90329328+erkutahmet@users.noreply.github.com>
2023-10-17 00:19:04 +0300

    created MVVM(Model-View-ViewModel) xcode project

---
+++ MVVM (Model-View-ViewModel)/MVVM (Model-View-ViewModel)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM (Model-View-ViewModel)
+//
+//  Created by Ahmet Erkut on 17.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
